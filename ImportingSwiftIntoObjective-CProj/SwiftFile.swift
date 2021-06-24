b7cc55f4379e7e8e338d8a5d39576803d7d699b5
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2021-06-24 22:14:14 +0700

    create swift Animator class

--- ImportingSwiftIntoObjective-CProj/SwiftFile.swift
+++ ImportingSwiftIntoObjective-CProj/SwiftFile.swift
@@ -6,7 +6,26 @@
 //
 
 import Foundation
+import Lottie
+import UIKit
 
 @objc class Human: NSObject {
     @objc var age: Int = 0
+}
+
+
+@objc class Animator: NSObject {
+    @objc func exe(with view: UIView)  {
+        let animationView = AnimationView()
+        let path = Bundle.main.path(forResource: "data",
+                                        ofType: "json") ?? ""
+        animationView.animation = Animation.filepath(path)
+        animationView.frame = view.bounds
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.animationSpeed = 0.5
+        view.addSubview(animationView)
+        view.bringSubviewToFront(animationView)
+        animationView.play()
+    }
 }

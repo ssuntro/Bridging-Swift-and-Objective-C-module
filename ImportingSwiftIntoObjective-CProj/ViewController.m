122e7e1d5d1e2f8d74f5f0cb3708a2ed587446ae
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2021-12-20 21:34:16 +0700

    (crashlytic)add crashbutton and complete setting

--- ImportingSwiftIntoObjective-CProj/ViewController.m
+++ ImportingSwiftIntoObjective-CProj/ViewController.m
@@ -22,7 +22,18 @@
     
     Animator * b = [[Animator alloc] init];
     [b exeWith: self.view];
+    [self crashButton];
+}
+- (void)crashButton {
+    UIButton* button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    button.frame = CGRectMake(20, 50, 100, 30);
+    [button setTitle:@"Test Crash" forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(crashButtonTapped:)
+        forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
 }
 
-
+-(IBAction)crashButtonTapped:(id)sender {
+    @[][1];
+}
 @end
